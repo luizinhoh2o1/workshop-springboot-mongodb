7a40cca0f7b7822f8b6aeaa9df10ad8bcb6faca2
Alessandro Luiz <alessandroluizfacul@gmail.com>
2021-09-16 21:28:20 -0300

    Starting the project

---
+++ src/test/java/com/alessandroluiz/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessandroluiz.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
