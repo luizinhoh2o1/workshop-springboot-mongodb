0e9e91ffc6192ad09e8b362ad2f84b774ac2fbe8
Alessandro Luiz <alessandroluizfacul@gmail.com>
2021-09-18 11:27:11 -0300

    Implemented findById and exception handling

---
+++ src/main/java/com/alessandroluiz/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.alessandroluiz.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.alessandroluiz.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Nao encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/alessandroluiz/workshopmongo/services/UserService.java
+++ src/main/java/com/alessandroluiz/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.alessandroluiz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alessandroluiz.workshopmongo.domain.User;
 import com.alessandroluiz.workshopmongo.repository.UserRepository;
+import com.alessandroluiz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
